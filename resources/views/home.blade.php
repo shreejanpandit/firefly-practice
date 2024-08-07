bc990cf597e26b0352182819cd461dea78ab4204
shreejanpandit <shreezanpandit@gmail.com>
2024-08-07 07:15:15 +0545

    [ADD]: added controller and route for movie

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<html>
+
+<head>
+    <title>Movies</title>
+</head>
+</body>
+<h1>test</h1>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [MovieController::class, 'index']);
+Route::get('/movies', [MovieController::class, 'create']);
+Route::post('/movies', [MovieController::class, 'store']);
+Route::get('/movies/{movie}/edit', [MovieController::class, 'edit']);
+Route::patch('/movies', [MovieController::class, 'update']);
+Route::delete('/movies/{movie}', [MovieController::class, 'destroy']);
