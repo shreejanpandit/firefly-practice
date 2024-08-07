186a0b72bf4a2df4293ceb5d1d9d7fc3cff30185
shreejanpandit <shreezanpandit@gmail.com>
2024-08-07 22:27:11 +0545

    [ADD]: addet model migration and controller for needed ones

---
+++ app/Http/Controllers/MedicalHistoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MedicalHistory;
+use Illuminate\Http\Request;
+
+class MedicalHistoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MedicalHistory $medicalHistory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MedicalHistory $medicalHistory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, MedicalHistory $medicalHistory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MedicalHistory $medicalHistory)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_08_07_163358_create_patients_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('contact', 60);
+            $table->date('dob');
+            $table->integer('age');
+            $table->string('email', 70);
+            $table->string('password', 255);
+            $table->string('photo')->nullable();
+            $table->enum('gender', ['male', 'female', 'other']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patients');
+    }
+};
