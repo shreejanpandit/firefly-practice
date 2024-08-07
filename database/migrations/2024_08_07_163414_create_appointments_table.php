594e0ab02840e5dfcd217b80716bbd96b6802382
shreejanpandit <shreezanpandit@gmail.com>
2024-08-07 22:40:33 +0545

    [ADD]: schema logic for appointment

--- database/migrations/2024_08_07_163414_create_appointments_table.php
+++ database/migrations/2024_08_07_163414_create_appointments_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Doctor;
+use App\Models\Patient;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('appointments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('patient_id')->constrained()->onDelete('cascade');
+            $table->foreignId('doctor_id')->constrained()->onDelete('cascade');
+            $table->integer('number');
+            $table->string('description')->nullable();
+            $table->enum('type', ['virtual', 'physical']);
+            $table->enum('status', ['active', 'pending', 'closed']);
+            $table->string('location')->default('manipal');
+            $table->date('date');
+            $table->time('time');
             $table->timestamps();
         });
     }
